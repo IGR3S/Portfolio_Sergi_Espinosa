282a9f9e4d7515d36b896f4d50c5b5ac1ccad901
Sete03 <sergiesbe@gmail.com>
2026-06-09 01:39:24 +0200

    estructura y header

---
+++ templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tartas Yoveia</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="icon" type="image/png" href="img/Logo.png">
+    <link rel="stylesheet" href="static/css/estilo.css">
+
+</head>
+<body>
+    <header>
+        <div class="header-inner">
+            <a href="index.php"><img class="logo" src="static/img/Logo.png" alt="Logo Sergi Espinosa"></a>
+            <nav>
+                <ul>
+                    <li><a href="index.php">INICIO</a></li>
+                    <li><a href="proyectos.php">PROYECTOS</a></li>
+                    <li><a href="contacto.php">CONTÁCTAME</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
